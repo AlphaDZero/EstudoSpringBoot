8f7b59670f22b2b616b492788a8f1c6f2d39e45e
AlphaDZero <andrebezerque@gmail.com>
2025-11-25 18:19:32 -0300

    A Base do Projeto

---
+++ brasileiras-ecommerce/src/main/java/com/brasileiras/brasileiras_ecommerce/BrasileirasEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.brasileiras.brasileiras_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileirasEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileirasEcommerceApplication.class, args);
+	}
+
+}
---
+++ brasileiras-ecommerce/src/test/java/com/brasileiras/brasileiras_ecommerce/BrasileirasEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasileiras.brasileiras_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileirasEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
